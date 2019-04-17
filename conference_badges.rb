7c456ab2ab53163de96262791cd3d658f9be7957
Nicole Dow <nikki.dow71@gmail.com>
2019-04-17 21:53:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,5 +3,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  
+  badge_messages = []
+  array.each do |name|
+    badge_messages << badge_maker(name)
+  end
+  return badge_messages
 end
